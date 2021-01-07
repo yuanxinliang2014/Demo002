e8150606822b94107caf673dace213c50289ffae
袁鑫亮 <yuanxinliang@jd.com>
2021-01-07 15:13:52 +0800

    commit001

--- Demo002/ViewController.m
+++ Demo002/ViewController.m
@@ -17,6 +17,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = UIColor.redColor;
+    self.view.backgroundColor = UIColor.blackColor;
 }
 
 
